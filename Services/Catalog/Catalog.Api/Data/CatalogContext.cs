c9eba4890b61bb7ee2cdced72ad0951f79a55488
awmxr <amir.m.n.1380@gmail.com>
2025-03-07 21:52:53 +0330

    seed data to mongo Products collection

---
+++ Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IMongoDatabase _database;
+        public IMongoCollection<Product> Products { get; }
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            _database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = _database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        
+    }
+}
---
+++ Services/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,54 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetSeedData());
+            }
+        }
+
+        private static IEnumerable<Product> GetSeedData()
+        {
+            return new List<Product>
+                        {
+                            new Product
+                            {
+                                Id = "5f47ac10b5e4c0001c8d4c1a",
+                                Name = "iPhone X",
+                                Category = "Smartphone",
+                                Description = "Apple iPhone X with 64GB memory",
+                                Summary = "Apple iPhone X",
+                                ImageFile = "iphonex.png",
+                                Price = 999.99M
+                            },
+                            new Product
+                            {
+                                Id = "5f47ac10b5e4c0001c8d4c1b",
+                                Name = "Samsung Galaxy S10",
+                                Category = "Smartphone",
+                                Description = "Samsung Galaxy S10 with 128GB memory",
+                                Summary = "Samsung Galaxy S10",
+                                ImageFile = "samsungs10.png",
+                                Price = 899.99M
+                            },
+                            new Product
+                            {
+                                Id = "5f47ac10b5e4c0001c8d4c1c",
+                                Name = "Google Pixel 4",
+                                Category = "Smartphone",
+                                Description = "Google Pixel 4 with 64GB memory",
+                                Summary = "Google Pixel 4",
+                                ImageFile = "googlepixel4.png",
+                                Price = 799.99M
+                            }
+                        };
+        }
+    }
+}
