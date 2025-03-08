ff9a2e38c897ce85851dab5b1512d98a1fab960a
awmxr <amir.m.n.1380@gmail.com>
2025-03-08 16:57:07 +0330

    Controller and repository for Discount

---
+++ Services/Discount/Discount.Api/Controllers/DiscountsController.cs
@@ -0,0 +1,66 @@
+﻿using Discount.Api.Entities;
+using Discount.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Discount.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        #region Constructor
+        private readonly IDiscountRepository _discountRepository;
+
+        public DiscountsController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        #endregion
+
+        #region GetDiscount
+
+        [HttpGet("{productName}",Name ="GetDiscount")]
+        [ProducesResponseType(typeof(Coupon),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var copun = await _discountRepository.GetDiscount(productName);
+            return Ok(copun);
+        }
+
+        #endregion
+
+        #region Create Discout
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromForm] Coupon coupon)
+        {
+            await _discountRepository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new {produuctName=coupon.ProductName}, coupon);
+        }
+        #endregion
+
+        #region Update Discout
+        [HttpPut]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> UpdateDiscount([FromForm] Coupon coupon)
+        {
+
+            return Ok(await _discountRepository.UpdateDiscount(coupon));
+        }
+        #endregion
+
+        #region Delete Discout
+        [HttpDelete("{productName}")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+
+            return Ok(await _discountRepository.DeleteDiscount(productName));
+        }
+        #endregion
+    }
+}
