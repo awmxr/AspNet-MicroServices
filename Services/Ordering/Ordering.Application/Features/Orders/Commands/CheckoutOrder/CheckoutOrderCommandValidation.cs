a11b3e44eef28552a3541d3462768434189b20ed
awmxr <amir.m.n.1380@gmail.com>
2025-03-10 11:39:06 +0330

    UpdateOrderCommand

---
+++ Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidation : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidation()
+        {
+            RuleFor(c => c.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exeed 50 character");
+
+            RuleFor(c => c.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(c=> c.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than 0");
+        }
+    }
+}
