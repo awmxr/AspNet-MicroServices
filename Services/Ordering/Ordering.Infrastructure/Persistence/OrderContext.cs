bdbed8348e3187060f7638a5f30e1311ae1a8717
awmxr <amir.m.n.1380@gmail.com>
2025-03-11 12:43:02 +0330

    Ordering.Infrastructure

---
+++ Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Order> Orders { get; set; }
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreateDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "Amir";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "Amir";
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public async Task SeedAsync(OrderContext orderContext , ILogger<OrderContextSeed> logger)
+        {
+            if (!await orderContext.Orders.AnyAsync())
+            {
+                await orderContext.Orders.AddRangeAsync(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+
+        public static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+
+            return new List<Order>
+            {
+                new Order
+                {
+                     UserName = "Amir",
+                     EmailAddress = "test@test.com",
+                     City = "krj",
+                     Country = "Iran",
+                     BankName = "mellat",
+                     FirstName = "Amir",
+                     LastName = "Marvasti",
+                     PaymentMethod = 1,
+                     RefCode = "121212121",
+                     TotalPrice = 10000,
+                }
+            };
+
+
+
+        }
+
+    }
+
+
+}
