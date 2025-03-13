41bc68574b3d5bb6989ee3609ba2ce4f75be89e4
awmxr <amir.m.n.1380@gmail.com>
2025-03-13 15:11:04 +0330

    Ocelot

---
+++ ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,22 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+// Configure logging
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("logging"));
+//builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddOcelot();
+var app = builder.Build();
+await app.UseOcelot();
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
